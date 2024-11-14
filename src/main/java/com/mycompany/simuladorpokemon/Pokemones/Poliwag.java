5351baa017cdaa9623c63ab2c47ee4c096ac6923
Anonimus <Anonimus@gmail.com>
2024-11-14 09:17:53 -0500

    se elimino vegeta y se agrego poliwag

---
+++ src/main/java/com/mycompany/simuladorpokemon/Pokemones/Poliwag.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.simuladorpokemon.Pokemones;
+
+import com.mycompany.simuladorpokemon.TipoPokemon;
+
+
+public class Poliwag extends Pokemon{
+    public Poliwag()
+    {
+        super("Poliwag",40,50, TipoPokemon.AGUA);
+    }
+}
--- src/main/java/com/mycompany/simuladorpokemon/SimuladorPokemon.java
+++ src/main/java/com/mycompany/simuladorpokemon/SimuladorPokemon.java
@@ -1,16 +1,16 @@
 package com.mycompany.simuladorpokemon;
 import com.mycompany.simuladorpokemon.Pokemones.Pikachu;
 import com.mycompany.simuladorpokemon.Pokemones.Pokemon;
-import com.mycompany.simuladorpokemon.Pokemones.Vegeta;
+import com.mycompany.simuladorpokemon.Pokemones.Vulpix;
 import java.util.Scanner;
 public class SimuladorPokemon {
 
     public static void main(String[] args) {
         Pokemon pikachu = new Pikachu();
-        Pokemon vegeta = new Vegeta();
+        Pokemon vulpix = new Vulpix();
         Batalla batalla = new Batalla();
         
-        Pokemon ganador = batalla.iniciarBatalla(pikachu, vegeta);
+        Pokemon ganador = batalla.iniciarBatalla(pikachu, vulpix);
         
         System.out.println("GANADOR: " + ganador.getNombre());
         Scanner scanner = new Scanner(System.in);
